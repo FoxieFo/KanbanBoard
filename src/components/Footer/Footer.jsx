903d1afe1278033f4fa9d453e680d38d014a8575
victoria <itsfoxiefo@gmail.com>
2024-08-21 20:25:06 +0200

    changed Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
+import s from './styles.module.scss'
+
 export default function Footer () {
     return (
-        <>
-            <p>Hi, I am Footer </p>
-        </>
+        <footer className={s.footer__container}>
+            <section className={s.footer__counter}>
+                <p>Active tasks:</p>
+                <p>Finished tasks:</p>
+            </section>
+            <p className={s.footer__author}>Kanban Board by NAME, YEAR</p>
+        </footer>
     );
 }
